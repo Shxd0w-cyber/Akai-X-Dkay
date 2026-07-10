207a10c6c9c9047107986504b18d54a61f82e945
Shxd0w-cyber <ashabeta2012@gmail.com>
2026-07-10 20:58:58 +0530

    Initial commit: Add main script.lua

---
+++ script.lua
@@ -0,0 +1,11 @@
+-- Akai X Dkay Script
+-- Main script entry point
+
+print("Script loaded successfully!")
+
+-- You can add your Lua code here
+local function main()
+    print("Running main function...")
+end
+
+main()
